fdae7dcfe023bb468a51355740633d6e789b663f
sonyali03 <45471193+sonyali03@users.noreply.github.com>
2019-01-04 14:11:24 -0800

    rdccdemo01-2

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	title := "Jenkins X golang http example - rdccdemo"
+	title := "Jenkins X golang http example - rdccdemo01-2"
 
 	from := ""
 	if r.URL != nil {
